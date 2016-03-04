9587401f995bfdca8701aebc0744c4af2ef8ad47
Renee Dudley <reneedudley@Renees-MacBook-Pro.local>
2016-03-03 18:40:17 -0600

    made controllers migrated table

---
+++ app/Http/Controllers/LanesController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class LanesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \App\Lane::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $lane = new \App\Lane;
+        $Lane->name = $request->name;
+        $lane->save();
+
+        return $lane;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return \App\Lane::find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $lane = \App\Lane::find($id);
+        $Lane->name = $request->name;
+        $lane->save();
+
+        return $lane;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $lane = \App\Lane::find($id);
+        $lane->delete();
+        return $lane;
+    }
+}
